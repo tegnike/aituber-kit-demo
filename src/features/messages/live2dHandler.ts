07c323eacc1de8af058d7413c3473f2f90f0c87d
tegnike <o3nike.teg.14@gmail.com>
2025-01-01 00:26:29 +0100

    お試し改修

--- src/features/messages/live2dHandler.ts
+++ src/features/messages/live2dHandler.ts
@@ -4,7 +4,7 @@ import settingsStore from '@/features/stores/settings'
 
 export class Live2DHandler {
   private static idleMotionInterval: NodeJS.Timeout | null = null // インターバルIDを保持
-  private static audioContext: AudioContext | null = null
+  public static audioContext: AudioContext | null = null
 
   static async speak(
     audioBuffer: ArrayBuffer,
